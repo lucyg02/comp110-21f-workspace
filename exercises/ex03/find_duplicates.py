09fe54a4e54eb9443001a2c3d17ef1ec2692461f
Lucy Grossmann <lucyg02@email.unc.edu>
2021-09-14 21:32:56 -0400

    ex03 probably maybe finished

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,20 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730391039"
+
+word: str = input("Enter a word: ")
+i: int = 0
+answer: str = ""
+
+while i < len(word):
+    j: int = i + 1
+    result: bool = bool(word[j] == word[i])
+    result_string: str = str(result)
+    if result is True and not False:
+        answer = result_string
+        i = len(word)
+    else:
+        answer = result_string
+    j += 1
+    i += 1
+print("Found duplicate: " + answer)
--- exercises/ex03/tar_heels.py
+++ exercises/ex03/tar_heels.py
@@ -1,6 +1,14 @@
 """An exercise in remainders and boolean logic."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730391039"
 
 
-# Begin your solution here...
+number: int = int(input("Enter an int: "))
+if ((number % 2) == 0) and ((number % 7) == 0):
+    print("TAR HEELS")
+if ((number % 2) == 0) and (not (number % 7) == 0):
+    print("TAR")
+if ((number % 7) == 0) and (not (number % 2) == 0):
+    print("HEELS")
+if (not (number % 2) == 0) and (not (number % 7) == 0):
+    print("CAROLINA")
